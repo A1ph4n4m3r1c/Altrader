7d1e888c45da3f79187d3ff3c9a94db7759a618c
A1ph4n4m3r1c <shadow.tym@gmail.com>
2014-02-26 01:52:09 -0500

    Update altrader.java

--- altrader.java
+++ altrader.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * A1ph4n4m3r1c
+ * 2-26-2014
+*/
 
 package altrader;
 
