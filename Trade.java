30a3d65152cb66a9c454b5ccdc7f813fee77b3ac
A1ph4n4m3r1c <shadow.tym@gmail.com>
2014-03-01 00:49:52 -0500

    Update Trade.java
    Added "Getfunds" method

--- Trade.java
+++ Trade.java
@@ -5,8 +5,22 @@ package altrader;
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
+import javax.json.*;
 
 public class Trade {
+    
+    public String outPut;
+    
+    public void getFunds() throws Exception{
+        URL getFunds = new URL("http://coinchan.org/Tests/btertest.php?pass=testpass&function=getFunds");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(getFunds.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+         System.out.println(inputLine);
+         in.close();
+    }
     
     public void getOpenOrders() throws Exception{
         URL getOrder = new URL("http://coinchan.org/Tests/btertest.php?pass=testpass&function=openOrders");
@@ -40,8 +54,35 @@ public class Trade {
         while ((inputLine = in.readLine()) != null)
          System.out.println(inputLine);
          in.close();
+        
+         }
+     
+         public void getPrice() throws IOException{
+             
+            System.out.println("Currency One:");
+            Scanner reader = new Scanner(System.in);
+            String currencyOne=reader.next();
+             System.out.println("Currency Two:");
+             Scanner readerTwo = new Scanner(System.in);
+            String currencyTwo=readerTwo.next();
+        
+
+             URL url = new URL("http://coinchan.org/Tests/altcointicker.php?USER=Admin&PASS=Lemonparty&CURRENCYONE="+currencyOne+"&CURRENCYTWO="+currencyTwo);
+             BufferedReader in = new BufferedReader(
+             new InputStreamReader(url.openStream()));
+
+                String inputLine;
+              while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+          
+          in.close();
+         }
          
-     }
          
-    }
+     
+
+        
+}
+
+
     
